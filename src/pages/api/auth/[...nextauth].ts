461e5d5c815fe31e372bfa405c4df4adaa2a52f6
Ittalo Steltter <ittalolz@gmail.com>
2022-04-18 14:05:13 -0400

    feat: save user in db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-import { redirect } from "next/dist/server/api-utils"
+
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   
@@ -8,16 +11,47 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user'
+        }
+      }
     }),
+    
   ],
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },  
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log(user)
-
-      return true
-    },
-    // async redirect({url, baseUrl})  {
-    //   return Promise.resolve(url)
-    // }  
+    async signIn({ user, account, profile, email, credentials }) {                
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: user.email } }            
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )          
+        );        
+        
+        return true;
+      }catch {
+        return false;
+      }          
+    },    
   }
 })
