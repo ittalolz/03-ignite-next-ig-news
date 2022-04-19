5e4ccf323d6908edfe2ee5f7e28b8a601f9645ff
Ittalo Steltter <ittalolz@gmail.com>
2022-04-19 17:43:14 -0400

    feat: cancel subscription update db

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,10 +23,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed',
-  'checkout.session.created',
-  'checkout.session.updated',
-  'checkout.session.deleted'
+  'checkout.session.completed',  
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -46,14 +45,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (relevantEvents.has(type)){
       try {
-        switch (type) {
+        switch (type) {          
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            )
+
+            break;
           case 'checkout.session.completed':
 
             const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription.toString(), 
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             )
             
             break;
